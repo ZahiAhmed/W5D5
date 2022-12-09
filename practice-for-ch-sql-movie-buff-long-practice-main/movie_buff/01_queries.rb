181cd9daf820c04dc9674742a2a936645ecd784e
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2022-12-09 16:03:22 -0500

    we have problem

--- practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
@@ -73,5 +73,5 @@ def movie_names_before_1940
   # improve performance for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
-
+  Movie.where(yr: 0...1940).pluck(:title)
 end
